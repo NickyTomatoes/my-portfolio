9c18c7816d51082b38ee0c857b24343c9277a582
NickyTomatoes <ncomito1221@gmail.com>
2022-08-04 19:36:21 -0400

    Updated Nav bar and functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import "./App.css";
+import React from "react";
+import "./index.css";
+import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Projects from "./Routes/Projects";
+import Contact from "./Routes/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>My name is Nick!</p>
-        <p className="App-link">Welcome!</p>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./NavbarStyles.css";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const clickHandler = () => {
+    setClick(!click);
+  };
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>
+          Comito<span>.</span>
+        </h1>
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={clickHandler}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
