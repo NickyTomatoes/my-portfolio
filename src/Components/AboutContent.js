faa39782ec969148b2b14c1eb194fd7a527e6801
NickyTomatoes <ncomito1221@gmail.com>
2022-08-13 13:03:31 -0400

    Swapped about imgs and hero img two background img and updated about section

--- src/Components/AboutContent.js
+++ src/Components/AboutContent.js
@@ -2,14 +2,24 @@ import "./AboutContent.css";
 
 import React from "react";
 import { Link } from "react-router-dom";
-import testimg from "../Assests/TestPlaceholderImg.jpg";
+import reactsticker from "../Assests/ReactSticker.jpg";
+import deskgraphic from "../Assests/DeskGraphic.jpg";
+import laptop from "../Assests/Laptop.jpg";
 
 const AboutContent = () => {
   return (
     <div className="about">
       <div className="left">
         <h1>Nicholas Comito</h1>
-        <p>Placeholder Text for description</p>
+        <p>
+          Passionate self-taught front end developer with a thirst for knowledge
+          and a drive to grow<span>.</span>
+        </p>
+        <p>
+          Born and raised in Philadelphia PA, a career change led me to take a
+          quick interest in programming, and I have since hit the ground running
+          <span>.</span>
+        </p>
         <Link to="/contact">
           <button className="btn">Contact</button>
         </Link>
@@ -17,10 +27,10 @@ const AboutContent = () => {
       <div className="right">
         <div className="img-container">
           <div className="img-stack top">
-            <img src={testimg} className="img" alt="true" />
+            <img src={deskgraphic} className="img" alt="true" />
           </div>
           <div className="img-stack bottom">
-            <img src={testimg} className="img" alt="true" />
+            <img src={laptop} className="img" alt="true" />
           </div>
         </div>
       </div>
