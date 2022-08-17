d3686cf11e4cef91c911f521aea0bad29f5228b8
NickyTomatoes <ncomito1221@gmail.com>
2022-08-17 14:22:35 -0400

    Adjust safari mobile browser height for hero img

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -3,28 +3,31 @@ import "./HeroImg.css";
 import React from "react";
 import IntroImg from "../Assests/desktop-img.jpg";
 import { Link } from "react-router-dom";
+import Div100vh from "react-div-100vh";
 
 const HeroImg = () => {
   return (
-    <div className="hero">
-      <div className="mask">
-        <img className="intro-img" src={IntroImg} alt="Intro Img" />
-      </div>
-      <div className="content">
-        <p>
-          Hi<span>!</span> I'm Nick.
-        </p>
-        <h1>Web Developer.</h1>
-        <div>
-          <Link to="/projects" className="btn">
-            Projects
-          </Link>
-          <Link to="/contact" className="btn btn-light">
-            Contact
-          </Link>
+    <Div100vh>
+      <div className="hero">
+        <div className="mask">
+          <img className="intro-img" src={IntroImg} alt="Intro Img" />
+        </div>
+        <div className="content">
+          <p>
+            Hi<span>!</span> I'm Nick.
+          </p>
+          <h1>Web Developer.</h1>
+          <div>
+            <Link to="/projects" className="btn">
+              Projects
+            </Link>
+            <Link to="/contact" className="btn btn-light">
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Div100vh>
   );
 };
 
