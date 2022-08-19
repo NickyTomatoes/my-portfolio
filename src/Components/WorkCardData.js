53c6cf618b1acb8f7a49ae214b18e65a2987c478
NickyTomatoes <ncomito1221@gmail.com>
2022-08-19 14:05:29 -0400

    Added correct imgs for projects

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -1,15 +1,17 @@
-import testimg from "../Assests/TestPlaceholderImg.jpg";
+import robstennis from "../Assests/Tennis-Screenshot.png";
+
+import brothersinlawn from "../Assests/BIL-Screenshot.png";
 
 const ProjectCardData = [
   {
-    imgsrc: testimg,
+    imgsrc: brothersinlawn,
     title: "Brothers in Lawn Landscaping",
     text: "Website for a local Landscaping company showcasing services offered and recent work.",
     view: "https://brothers-in-lawn.netlify.app/",
     source: "https://github.com/NickyTomatoes/Brothers-in-Lawn",
   },
   {
-    imgsrc: testimg,
+    imgsrc: robstennis,
     title: "Rockin With Rob Tennis",
     text: "Website for a local tennis coach showcasing training and services available.",
     view: "https://rockinwithrob.netlify.app/",
